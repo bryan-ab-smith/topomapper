38959df147f67677040dfd9cbe1ebffbe40cbe63
Bryan Smith <34717084+bryan-ab-smith@users.noreply.github.com>
2021-03-09 08:51:55 +1000

    Small tweak to the debug server.

--- debug_server.py
+++ debug_server.py
@@ -1,4 +1,5 @@
 import platform
+import sys
 
 try:
     # Python 3
@@ -16,16 +17,19 @@ except ImportError:
 
 pyVer = int(platform.python_version_tuple()[0])
 port = 8000
-if pyVer < 3:
-    # Credit to https://docs.python.org/2/library/simplehttpserver.html
-    Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
-    httpd = SocketServer.TCPServer(('', port), Handler)
-    print('Running at http://localhost:{}'.format(port))
-    httpd.serve_forever()
-else:
-    # Credit to https://www.afternerd.com/blog/python-http-server/
-    Handler = http.server.SimpleHTTPRequestHandler
-
-    with socketserver.TCPServer(('', port), Handler) as httpd:
-        print('Running at http://localhost:{}'.format(port))
+try:
+    if pyVer < 3:
+        # Credit to https://docs.python.org/2/library/simplehttpserver.html
+        Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+        httpd = SocketServer.TCPServer(('', port), Handler)
+        print('Running at http://localhost:{}. Press Ctrl-C to exit.'.format(port))
         httpd.serve_forever()
+    else:
+        # Credit to https://www.afternerd.com/blog/python-http-server/
+        Handler = http.server.SimpleHTTPRequestHandler
+
+        with socketserver.TCPServer(('', port), Handler) as httpd:
+            print('Running at http://localhost:{}. Press Ctrl-C to exit.'.format(port))
+            httpd.serve_forever()
+except KeyboardInterrupt:
+    sys.exit(0)
