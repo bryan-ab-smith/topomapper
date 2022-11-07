c5c0e3f9add423fe5e07b3b05f76f3806cf007f6
Bryan Smith <bryanabsmith@gmail.com>
2022-11-07 14:59:08 +1000

    Updated Archer Street.

--- debug_server.py
+++ debug_server.py
@@ -1,11 +1,11 @@
 import platform
-from signal import signal, SIGPIPE, SIG_DFL 
+# from signal import signal, SIGPIPE, SIG_DFL
 import sys
 
 # https://linuxpip.org/broken-pipe-python-error/#Avoid_Errno_32_Broken_pipe_by_ignoring_SIGPIPE
 # Broken pipe errors were thrown up for no particular reason.
 # This suppresses these.
-signal(SIGPIPE, SIG_DFL)
+# signal(SIGPIPE, SIG_DFL)
 
 try:
     # Python 3
@@ -30,14 +30,14 @@ try:
         httpd = SocketServer.TCPServer(('', port), Handler)
         # https://stackoverflow.com/a/31746811
         httpd.allow_reuse_address = True
-        print('Running at http://localhost:{}. Press Ctrl-C to exit.'.format(port))
+        print(f'Running at http://localhost:{port}. Press Ctrl-C to exit.')
         httpd.serve_forever()
     else:
         # Credit to https://www.afternerd.com/blog/python-http-server/
         Handler = http.server.SimpleHTTPRequestHandler
 
         with socketserver.TCPServer(('', port), Handler) as httpd:
-            print('Running at http://localhost:{}. Press Ctrl-C to exit.'.format(port))
+            print(f'Running at http://localhost:{port}. Press Ctrl-C to exit.')
             httpd.serve_forever()
 except KeyboardInterrupt:
     sys.exit(0)
